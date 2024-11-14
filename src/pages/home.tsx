6c42004b206bae5dddfe0e4845c988d881f6f509
stuartmills <stuartmills01@protonmail.com>
2024-11-14 01:18:26 -0600

    Update

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -132,11 +132,11 @@ const HomePage: React.FC = () => {
           <p className="text-xl text-gray-400 mb-4 text-center">Tech We Use</p>
           <div className="flex justify-between items-center w-full">
             <img src="/python.png" alt="Python" style={{ height: '30px' }} />
-            <img src="/react.png" alt="React" style={{ height: '30px' }} />
+            <img src="/react.png" alt="React" style={{ height: '36px' }} />
             <img src="/openai.png" alt="OpenAI" style={{ height: '30px' }} />
             <img src="/segmind.png" alt="Segmind" style={{ height: '30px' }} />
-            <img src="/vercel.png" alt="Vercel" style={{ height: '30px' }} />
-            <img src="/gcp.png" alt="GCP" style={{ height: '30px' }} />
+            <img src="/vercel.png" alt="Vercel" style={{ height: '26px' }} />
+            <img src="/gcp.png" alt="GCP" style={{ height: '26px' }} />
           </div>
         </section>
 
